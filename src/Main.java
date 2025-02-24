bb7bf87918ce2cac1e67de2731a26d4cf951133c
Test <shpakovskiyam@gmail.com>
2025-02-24 21:31:07 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,34 @@ public class Main {
 
         String knockKnock = "Тук-Тук";
         String whoIs = "\tКто там";
+        int outerCycle = input();
+        int innerCycle = input();
 
-        int outerCycle = -1;
-        int innerCycle = -1;
+        for (int i = 0; i < outerCycle; i++) {
+            System.out.println(knockKnock);
 
-        Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNextInt()) {
-            outerCycle = scanner.nextInt();
-        }
-        if (scanner.hasNextInt()) {
-            innerCycle = scanner.nextInt();
-        }
-
-        if (outerCycle >= 0 && innerCycle >= 0) {
-            while (true) {
-                for (int i = 0; i < outerCycle; i++) {
-                    System.out.println(knockKnock);
-
-                    for (int j = 0; j < innerCycle; j++) {
-                        System.out.println(whoIs); //update_tst_br1
-                    }
-                }
-                break;
+            for (int j = 0; j < innerCycle; j++) {
+                System.out.println(whoIs); //update_tst_br1
             }
-        } else {
-            System.out.println("Expected two digits on input. Received letter or negative value. Restart required.");
         }
-
 
     }
+
+    public static int input() {
+        int inputData = -1;
+
+        while (inputData == -1) {
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                inputData = scanner.nextInt();
+            } else {
+                System.out.println("Expected two digits on input. Received letter or negative value. Try again.");
+            }
+        }
+
+        return inputData;
+    }
+
 }
 
 
