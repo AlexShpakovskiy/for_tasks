41e68834e159886495443d391b0d453c9c26127e
Test <shpakovskiyam@gmail.com>
2025-02-24 21:51:55 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,32 @@ public class Main {
 
         String knockKnock = "Тук-Тук";
         String whoIs = "\tКто там";
-        int outerCycle = input();
-        int innerCycle = input();
+        int[] cycle = new int[2];
+        cycle = input();
 
-        for (int i = 0; i < outerCycle; i++) {
+        for (int i = 0; i < cycle[0]; i++) {
             System.out.println(knockKnock);
 
-            for (int j = 0; j < innerCycle; j++) {
+            for (int j = 0; j < cycle[1]; j++) {
                 System.out.println(whoIs); //update_tst_br1
             }
         }
 
     }
 
-    public static int input() {
-        int inputData = -1;
+    public static int[] input() {
+        int[] inputData = new int[2];
 
-        while (inputData == -1) {
-            Scanner scanner = new Scanner(System.in);
-            if (scanner.hasNextInt()) {
-                inputData = scanner.nextInt();
-            } else {
-                System.out.println("Expected two digits on input. Received letter or negative value. Try again.");
+        for (int i = 0; i < 2; i++) {
+
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                if (scanner.hasNextInt()) {
+                    inputData[i] = scanner.nextInt();
+                    break;
+                } else {
+                    System.out.println("Expected two digits on input. Received letter or negative value. Try again.");
+                }
             }
         }
 
